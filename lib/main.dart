fbd6d2300c5ab2fab996cd50881dc3fcd0d753c2
Ariana Camargos <arianacamargos16@gmail.com>
2022-11-28 22:32:52 -0300

    feat: insere page na main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rick_morty/views/http_page.dart';
+
+import 'controllers/http/http_bindings.dart';
 
 void main() {
   runApp(const HomePage());
@@ -10,19 +11,17 @@ void main() {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override 
-  Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context){
     return GetMaterialApp(
-      getPages: const [
-        // GetPage(
-        //   name: '/details',
-        //   page: () => const ,
-        //   binding: DetailsPage(),
-        // ),
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => const HttpPage(),
+          binding: HttpBindings(),
+        ),
       ],
-
       title: 'API Rick and Morty',
-      home: const HttpPage(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: const ColorScheme.light(
