07141b49d8bce4fd489fb1b4c6908360ddc4dd77
Ariana Camargos <arianacamargos16@gmail.com>
2022-11-28 22:45:19 -0300

    style: format

--- lib/views/http_page.dart
+++ lib/views/http_page.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:get/get.dart';
-import 'package:rick_morty/controllers/http_controller.dart';
-// ignore: unused_import
+import 'package:rick_morty/controllers/http/http_controller.dart';
 import 'package:rick_morty/models/character_model.dart';
 
 class HttpPage extends GetView<HttpController> {
   const HttpPage({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -30,21 +27,23 @@ class HttpPage extends GetView<HttpController> {
           ),
         ],
       ),
-      body: controller.obx((state) => ListView.builder(
-            itemCount: state.lenght,
+      body: controller.obx(
+        (state) => ListView.builder(
+            itemCount: state?.lenght,
             itemBuilder: (_, index) {
               final CharacterModel item = state[index];
               return ListTile(
                 title: Text('${item.name}'),
-               // subtitle: Text('${item.species}'),
+                // subtitle: Text('${item.species}'),
               );
-            }), onError: (error) =>  
-        const Center(
+            }),
+        onError: (error) => const Center(
           child: Text(
             'Erro ao carregar página! ',
           ),
-        )
-      , onLoading: const CircularProgressIndicator()), 
+        ),
+        onLoading: const CircularProgressIndicator(),
+      ),
     );
   }
 }
