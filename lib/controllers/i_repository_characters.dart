e7c63459a6f6debf1fa334fb01bf6d0926a96449
Ariana Camargos <arianacamargos16@gmail.com>
2023-01-25 00:02:06 -0300

    feat: deleta arquivos

--- lib/controllers/i_repository_characters.dart
+++
@@ -1,5 +0,0 @@
-import 'package:rick_morty/models/character_model.dart';
-
-abstract class ICharacterRepository {
-  Future<List<CharacterModel>> findAllCharacters();
-}
--- lib/controllers/i_repository_episodes.dart
+++
@@ -1,5 +0,0 @@
-import '../models/episodes_model.dart';
-
-abstract class IEpisodesRepository {
-  Future<List<EpisodesModel>> findAllEpisodes();
-}
