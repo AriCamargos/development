6ef9e1e8a5ac2132ef57fa2ff11431adebb1d0f2
Ariana Camargos <arianacamargos16@gmail.com>
2023-06-29 22:45:27 -0300

    feat: add datasource character

--- lib/external/datasources/rick_morty_datasource.dart
+++ lib/external/datasources/rick_morty_datasource.dart
@@ -4,16 +4,27 @@ import 'package:rick_morty/domain/entity/character_entity.dart';
 import 'package:rick_morty/infra/model/character_model.dart';
 
 class RickMortyDatasource {
-  Future<CharacterEntity> getCharacterUrl() async {
-    final url = Uri.parse("https://rickandmortyapi.com/api/character");
-    final response = await http.get(url);
+  final String baseUrl = "https://rickandmortyapi.com/api/";
 
-    if (response.statusCode == 200) {
-      return CharacterModel.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Erro ao carregar');
+  Future<List<CharacterEntity>> getCharacterUrl() async {
+    try {
+      final response = await http.get(Uri.parse('${baseUrl}character'));
+
+      if (response.statusCode == 200) {
+        final jsonResult = json.decode(response.body);
+        final charactersList = jsonResult['results'] as List<dynamic>;
+
+        List<CharacterEntity> characters = charactersList
+            .map((characterJson) => CharacterModel.fromJson(characterJson))
+            .toList();
+
+        return characters;
+      } else {
+        throw Exception('Erro ao carregar');
+      }
+    } catch (e) {
+      throw Exception('Erro ao carregar caracter $e');
     }
-    
   }
-  //return CharacterModel.fromJson();
 }
+
