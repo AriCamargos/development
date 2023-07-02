ef365be63254c9af5fa9324fb2d2ef7c2b61873f
Ariana Camargos <arianacamargos16@gmail.com>
2023-07-02 18:43:18 -0300

    feat: model

---
+++ lib/infra/model/location_model.dart
@@ -0,0 +1,18 @@
+import 'package:rick_morty/domain/entity/location_entity.dart';
+
+class LocationModel extends LocationEntity {
+  const LocationModel({
+    required String name,
+    required String url,
+  }) : super(
+          name: name,
+          url: url,
+        );
+
+  factory LocationModel.fromJson(Map<String, dynamic> json) {
+    return LocationModel(
+      name: json['name'],
+      url: json['url'],
+    );
+  }
+}
