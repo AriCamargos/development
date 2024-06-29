ae7389faf90d325e5333873bb47b5d0ed468eef8
AriCamargos <arianacamargos16@gmail.com>
2024-06-29 16:25:49 -0300

    feat: page principal

--- lib/presenter/pages/character_page.dart
+++ lib/presenter/pages/character_page.dart
@@ -1,32 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 import 'package:rick_morty/presenter/pages/character_state.dart';
 
 class CharacterPage extends StatefulWidget {
-  const CharacterPage({super.key});
+  const CharacterPage({
+    super.key,
+  });
 
   @override
   State<CharacterPage> createState() => _CharacterPageState();
 }
 
 class _CharacterPageState extends State<CharacterPage> {
-  /* Future<List> getCharacterUrl() async {
-    final url = Uri.parse("https://rickandmortyapi.com/api/character");
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception('Erro ao carregar');''
-    }
-  }*/
-
-  /*late Future<CharacterEntity> futureCharacter;
-
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    //futureCharacter = getCharacterUrl();
- }*/
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) {
+        context.read<CharacterState>().loadingCharacter(page: 1, count: 20);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,41 +29,56 @@ class _CharacterPageState extends State<CharacterPage> {
         backgroundColor: Colors.amber,
         title: const Text('Api Rick e Morty'),
       ),
-      body: FutureBuilder<List>(
-       // future: getCharacter(),
-        // futureCharacter,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('${snapshot.error}'),
+      body: Consumer<CharacterState>(
+        builder: (_, state, child) {
+          if (state.isLoading && state.character.isEmpty) {
+            return const Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 5.0,
+                color: Colors.pink,
+              ),
             );
           }
-          if (snapshot.hasData) {
-            return CharacterPage();
-            /*  ListView.builder(
-              itemCount: snapshot.data!.length,
+          if (state.error != null) {
+            return Center(
+              child: Text('${state.error}'),
+            );
+          }
+          if (state.character.isEmpty) {
+            return const Center(
+              child: Text('Lista vazia'),
+            );
+          }
+
+          return NotificationListener<ScrollNotification>(
+            onNotification: (ScrollNotification scrollInfo) {
+              if (!state.isLoading &&
+                  scrollInfo.metrics.pixels ==
+                      scrollInfo.metrics.maxScrollExtent) {
+                state.loadingCharacter(
+                  page: (state.character.length ~/ 20) + 1,
+                  count: 20,
+                );
+                return true;
+              }
+              return false;
+            },
+            child: ListView.builder(
+              itemCount: state.character.length,
               itemBuilder: (context, index) {
-                var character = snapshot.data![index];
+                var character = state.character[index];
                 return Card(
                   elevation: 5,
                   margin: const EdgeInsets.only(top: 5, left: 5, right: 5),
                   child: ListTile(
                     leading: CircleAvatar(
+                      backgroundImage: NetworkImage(character!.image),
                       backgroundColor: Colors.black,
-                      child: Icon(
-                        character.image,
-                      ),
                     ),
                     title: Text(character.name),
                   ),
                 );
               },
-            );*/
-          }
-          return const Center(
-            child: CircularProgressIndicator(
-              strokeWidth: 5.0,
-              color: Colors.pink,
             ),
           );
         },
