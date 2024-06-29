7187f6c09e2ee78071111bdb8044699a5dc35014
AriCamargos <arianacamargos16@gmail.com>
2024-06-29 16:24:06 -0300

    feat: datasource

--- lib/external/datasources/character_datasource.dart
+++ lib/external/datasources/character_datasource.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import 'package:rick_morty/domain/entity/character_entity.dart';
-import 'package:rick_morty/infra/model/character_model.dart';
+import 'package:rick_morty/infra/mappers/character_mapper.dart';
 
 import '../../infra/datasource/character_datasource.dart';
 
@@ -12,28 +12,29 @@ class CharacterDatasource extends ICharacterDatasource {
 
   @override
   Future<List<CharacterEntity>> getCharacter({
-    int? page,
-    int? count,
-    String? next,
     String? prev,
+    String? next,
+    required int page,
+    required int count,
   }) async {
-   // try {
-   //   final response = await http.get(Uri.parse('${baseUrl}character'));
+    try {
+      final url = next ?? '$baseUrl/character?page=$page&count=$count';
+      final response = await http.get(Uri.parse(url));
 
-    //  if (response.statusCode == 200) {
-    //    final jsonResult = json.decode(response.body);
-     //   final charactersList = jsonResult['results'] as List<dynamic>;
+      if (response.statusCode == 200) {
+        final jsonResult = json.decode(response.body);
+        final charactersList = jsonResult['results'] as List<dynamic>;
 
-       // List<CharacterEntity> characters = charactersList
-        //    .map((characterJson) => CharacterModel.fromJson(characterJson))
-         //   .toList();
+        List<CharacterEntity> characters = charactersList
+            .map((characterJson) => CharacterMapper.fromMap(characterJson))
+            .toList();
 
-       //return characters;
-    //  } else {
-    //    throw Exception('Erro ao carregar');
-    //  }
-    //} catch (e) {
-   //   throw Exception('Erro ao carregar caracter $e');
-    //}
+        return characters;
+      } else {
+        throw Exception('Erro ao carregar');
+      }
+    } catch (e) {
+      throw Exception('Erro ao carregar caracter $e');
+    }
   }
 }
