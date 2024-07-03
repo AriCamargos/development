2d2e88e19d973d26837951fef78e78bf6762cd7e
AriCamargos <arianacamargos16@gmail.com>
2024-07-02 23:16:53 -0300

    feat: enum status

---
+++ lib/domain/enum/enum_status.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+enum StatusEnum {
+  alive(color: Color(0xFF388E3C), status: 'Alive'),
+  dead(color: Color(0xFFE53935), status: 'Dead'),
+  unknown(color: Color(0xFF4A148C), status: 'Unknown');
+
+  final Color? color;
+  final String? status;
+
+  const StatusEnum({this.color, this.status});
+
+  static getNameStatus(String status) {
+    StatusEnum.values
+        .firstWhere(
+          (element) => status == status,
+        )
+        .status;
+  }
+
+  static getColor(Color color) {
+    StatusEnum.values
+        .firstWhere(
+          (element) => color == color,
+        )
+        .color;
+  }
+}
